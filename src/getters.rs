bfae353a8d33003c494c668860807f10ce5af08d
Charlie <lambopuggy@gmail.com>
2024-12-19 13:19:17 +0000

    updated cargo.toml to be correct, working on condencing index based station playing

--- src/getters.rs
+++ src/getters.rs
@@ -1,10 +1,11 @@
 use radiobrowser::blocking::RadioBrowserAPI;
-use radiobrowser::{ApiStation, ApiTag };
-use radiobrowser::{StationOrder,TagOrder};
+use radiobrowser::{ApiCountry, ApiStation, ApiTag};
+use radiobrowser::{CountryOrder, StationOrder, TagOrder};
 use std::error::Error;
 
-pub fn get_top_stations(api: &RadioBrowserAPI) -> Result<Vec<ApiStation>, Box<dyn Error>>{
-    let stations = api.get_stations()
+pub fn get_top_stations(api: &RadioBrowserAPI) -> Result<Vec<ApiStation>, Box<dyn Error>> {
+    let stations = api
+        .get_stations()
         .limit("100")
         .order(StationOrder::Clickcount)
         .reverse(true)
@@ -13,11 +14,12 @@ pub fn get_top_stations(api: &RadioBrowserAPI) -> Result<Vec<ApiStation>, Box<dy
     Ok(stations)
 }
 
-
-pub fn get_stations_by_name(api: &RadioBrowserAPI, query: &String) -> Result<Vec<ApiStation>, Box<dyn Error>>{
-
-
-    let stations = api.get_stations()
+pub fn get_stations_by_name(
+    api: &RadioBrowserAPI,
+    query: &String,
+) -> Result<Vec<ApiStation>, Box<dyn Error>> {
+    let stations = api
+        .get_stations()
         .name(query)
         .limit("100") // arbitary just here incase query is too generic
         .order(StationOrder::Votes)
@@ -27,30 +29,32 @@ pub fn get_stations_by_name(api: &RadioBrowserAPI, query: &String) -> Result<Vec
     Ok(stations)
 }
 
-
-
-pub fn get_presets(api: &RadioBrowserAPI, presets: &[&str]) -> Result<Vec<ApiStation>, Box<dyn std::error::Error>> {
-
+pub fn get_presets(
+    api: &RadioBrowserAPI,
+    presets: &[&str],
+) -> Result<Vec<ApiStation>, Box<dyn std::error::Error>> {
     let mut stations: Vec<ApiStation> = Vec::with_capacity(presets.len());
-    for &preset in presets{
-        let station = api.get_stations()
-        .name(preset)
-        .name_exact(true)
-        .order(StationOrder::Clickcount)
-        .limit("1") // don't want to do this but may speed things up
-        .send()?;
+    for &preset in presets {
+        let station = api
+            .get_stations()
+            .name(preset)
+            .name_exact(true)
+            .order(StationOrder::Clickcount)
+            .reverse(true)
+            .limit("1") // don't want to do this but may speed things up
+            .send()?;
         stations.push(station[0].clone());
     }
-
 
     Ok(stations)
 }
 
-
-pub fn get_stations_by_tag(api: &RadioBrowserAPI, query: &String) -> Result<Vec<ApiStation>, Box<dyn Error>>{
-
-
-    let stations = api.get_stations()
+pub fn get_stations_by_tag(
+    api: &RadioBrowserAPI,
+    query: &String,
+) -> Result<Vec<ApiStation>, Box<dyn Error>> {
+    let stations = api
+        .get_stations()
         .tag(query)
         .limit("100") // arbitary just here incase query is too generic
         .order(StationOrder::Votes)
@@ -60,13 +64,42 @@ pub fn get_stations_by_tag(api: &RadioBrowserAPI, query: &String) -> Result<Vec<
     Ok(stations)
 }
 
-pub fn get_tags(api: &RadioBrowserAPI, limit: &str) -> Result<Vec<ApiTag>, Box<dyn Error>>{
-    let tags = api.get_tags()
+pub fn get_tags(api: &RadioBrowserAPI, limit: &str) -> Result<Vec<ApiTag>, Box<dyn Error>> {
+    let tags = api
+        .get_tags()
         .order(TagOrder::StationCount)
         .reverse(true)
-        .limit(limit) // replace with dynamic 
+        .limit(limit) // replace with dynamic
         .send()?;
 
     Ok(tags)
-    
+}
+
+pub fn get_countries(
+    api: &RadioBrowserAPI,
+    limit: &str,
+) -> Result<Vec<ApiCountry>, Box<dyn Error>> {
+    let countries = api
+        .get_countries()
+        .order(CountryOrder::StationCount)
+        .reverse(true)
+        .limit(limit) // replace with dynamic
+        .send()?;
+
+    Ok(countries)
+}
+
+pub fn get_stations_by_country(
+    api: &RadioBrowserAPI,
+    query: &String,
+) -> Result<Vec<ApiStation>, Box<dyn Error>> {
+    let stations = api
+        .get_stations()
+        .country(query)
+        .limit("100") // arbitary just here incase query is too generic
+        .order(StationOrder::Votes)
+        .reverse(true)
+        .send()?;
+
+    Ok(stations)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
-
 mod getters;
-use crate::getters::{get_top_stations,
-                    get_stations_by_tag,
-                    get_stations_by_name,
-                    get_presets,
-                    get_tags,
-                };
+use crate::getters::{
+    get_countries, get_presets, get_stations_by_country, get_stations_by_name, get_stations_by_tag,
+    get_tags, get_top_stations,
+};
 
 use std::process::Command;
+use std::string;
 
 use radiobrowser::blocking::RadioBrowserAPI;
-use radiobrowser::{ApiStation};
+use radiobrowser::{ApiCountry, ApiStation, ApiTag};
 
 use std::error::Error;
 
-fn create_api_instance() -> Result<RadioBrowserAPI, Box<dyn Error>>{
+fn create_api_instance() -> Result<RadioBrowserAPI, Box<dyn Error>> {
     //create api client instance here and pass through to relevent functions
     //really just to prevent creating too many clients and the server responding with a 429 error
     Ok(RadioBrowserAPI::new()?)
 }
- 
 
-
-
-fn station_select(stations: Vec<ApiStation>){
+fn station_select(stations: Vec<ApiStation>) {
     //segement for selecting stations from a given list
 
     for (index, station) in stations.iter().enumerate() {
@@ -31,12 +26,16 @@ fn station_select(stations: Vec<ApiStation>){
     }
     println!("Enter the number of the station to play:");
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Failed to read input");
-    let input = input.trim().parse::<usize>().unwrap_or(0);
-    if input > 0 && input <= stations.len() {
-        play_station(&stations[input - 1]);
-    } else {
-        println!("Invalid input");
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+    match input.trim().parse::<usize>() {
+        Ok(num) if num > 0 && num <= stations.len() => {
+            play_station(&stations[num - 1]);
+        }
+        _ => {
+            print!("Invalid Input")
+        }
     }
 }
 
@@ -50,15 +49,71 @@ fn play_station(station: &ApiStation) {
         .expect("Failed to spawn mpv process");
 }
 
-fn generic_query()-> String{
+fn generic_query() -> String {
     // not elegant, needs better handling of return within the confines of rust
     let mut station_query = String::new();
     println!("Enter the station name:");
-    std::io::stdin().read_line(&mut station_query)
+    std::io::stdin()
+        .read_line(&mut station_query)
         .expect("Failed to read input");
 
     let station_query = station_query.trim();
     station_query.to_string()
+}
+
+enum QueryEntity {
+    Tag(Vec<ApiTag>),
+    Country(Vec<ApiCountry>),
+}
+
+impl QueryEntity {
+    fn args_query(&self) {
+        let mut test = false;
+        match self {
+            QueryEntity::Tag(tag) => {
+                let data = tag;
+            }
+            QueryEntity::Country(country) => {
+                test = true;
+                let data = country;
+            }
+            _ => {
+                let data: Vec<ApiTag> = Vec::new();
+            }
+        }
+        println!("hello");
+        for (index, tag) in data.iter().enumerate() {
+            println!("{}: {}", index + 1, tag.name);
+        }
+    }
+}
+
+fn args_query2(data: Vec<ApiTag>) -> Result<(), Box<dyn Error>> {
+    // Print tags with their indices
+
+    for (index, tag) in data.iter().enumerate() {
+        println!("{}: {}", index + 1, tag.name);
+    }
+
+    println!("Enter the number you want to search:");
+
+    // Read user input
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    // Parse input and handle potential errors
+    match input.trim().parse::<usize>() {
+        Ok(num) if num > 0 && num <= data.len() => {
+            let stations = get_stations_by_country(api, &data[num - 1].name)?;
+            station_select(stations);
+        }
+        _ => {
+            println!("Invalid input");
+        }
+    }
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -67,9 +122,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         "BBC Radio 1",
         "Radio Paradise",
         "Capital FM London",
-        "Radio X"
+        "Radio X",
     ];
-    
+
     match create_api_instance() {
         //Pass api instance if we can successfully connect
         Ok(api) => {
@@ -81,25 +136,23 @@ fn main() -> Result<(), Box<dyn Error>> {
                 println!("2. Search station by name");
                 println!("3. Search tags");
                 println!("4. Search top 100 stations");
-                println!("5. exit");
-        
+                println!("5. Search country");
+
                 let mut input = String::new();
-                std::io::stdin().read_line(&mut input).expect("Failed to read input");
+                std::io::stdin()
+                    .read_line(&mut input)
+                    .expect("Failed to read input");
                 let input = input.trim();
-        
+
                 match input {
                     "1" => {
-
                         station_select(preset_stations.clone());
                     }
                     "2" => {
-
                         let stations = get_stations_by_name(api_ref, &generic_query())?;
                         station_select(stations);
-                    
                     }
-                    "3" =>{
-
+                    "3" => {
                         let tags = get_tags(api_ref, "30")?;
 
                         // Print tags with their indices
@@ -108,10 +161,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                         }
 
                         println!("Enter the number of the tag you want to search:");
-                        
+
                         // Read user input
                         let mut input = String::new();
-                        std::io::stdin().read_line(&mut input)
+                        std::io::stdin()
+                            .read_line(&mut input)
                             .expect("Failed to read input");
 
                         // Parse input and handle potential errors
@@ -124,24 +178,49 @@ fn main() -> Result<(), Box<dyn Error>> {
                                 println!("Invalid input");
                             }
                         }
-
-                    } 
-                    "4" => {
-                        let stations =get_top_stations(api_ref)?;
-                        station_select(stations);
-
                     }
-                    "5" => break,
+                    "4" => {
+                        let stations = get_top_stations(api_ref)?;
+                        station_select(stations);
+                    }
+                    "5" => {
+                        let countries = get_countries(api_ref, "30")?;
+                        let test: Box<dyn Processable> = Box::new(countries);
+                        // Print tags with their indices
+                        //for (index, tag) in countries.iter().enumerate() {
+                        //    println!("{}: {}", index + 1, tag.name);
+                        //}
+
+                        //println!("Enter the number of the tag you want to search:");
+
+                        // Read user input
+                        //let mut input = String::new();
+                        //std::io::stdin()
+                        //    .read_line(&mut input)
+                        //    .expect("Failed to read input");
+
+                        // Parse input and handle potential errors
+                        //match input.trim().parse::<usize>() {
+                        //    Ok(num) if num > 0 && num <= countries.len() => {
+                        //        let stations =
+                        //            get_stations_by_country(api_ref, &countries[num - 1].name)?;
+                        //        station_select(stations);
+                        //    }
+                        //    _ => {
+                        //        println!("Invalid input");
+                        //    }
+                        //}
+                    }
+
+                    "6" => break,
                     _ => println!("Invalid option"),
                 }
             }
-            
         }
         Err(e) => {
             eprintln!("Error: {}", e);
-        }        
+        }
     }
 
     Ok(())
 }
-
